3951f17d168451181fdb12b47eb9722fe9e9ecab
Sandhya <20344136+sandyspring13@users.noreply.github.com>
2019-12-02 22:46:34 -0500

    css changes

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ var express = require("express"),
 
 
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(__dirname+ "/public"));
 app.set("view engine", "ejs");
 app.use(flash());
 
